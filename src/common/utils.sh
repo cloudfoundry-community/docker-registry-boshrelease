db802983730051b142eb27a0f83f5627116577c0
James Hunt <james@niftylogic.com>
2017-01-26 17:03:50 -0500

    Fix cf_release Security Vulnerability
    Cargo-culting from cf_release nicked us with this vuln:
    https://docs.google.com/document/d/1MOJPOR_fNjvhu2zcEvLm632Dmd2HGp71UCrSmyOHcFs/pub
    
    Fixed.

--- src/common/utils.sh
+++ src/common/utils.sh
@@ -1,8 +1,7 @@
 
 mkdir -p /var/vcap/sys/log
 
-exec > >(tee -a >(logger -p user.info -t vcap.$(basename $0).stdout) | awk -W interactive '{lineWithDate="echo [`date +\"%Y-%m-%d %H:%M:%S%z\"`] \"" $0 "\""; system(lineWithDate)  }' >>/var/vcap/sys/log/$(basename $0).log)
-exec 2> >(tee -a >(logger -p user.error -t vcap.$(basename $0).stderr) | awk -W interactive '{lineWithDate="echo [`date +\"%Y-%m-%d %H:%M:%S%z\"`] \"" $0 "\""; system(lineWithDate)  }' >>/var/vcap/sys/log/$(basename $0).err.log)
+exec >>/var/vcap/sys/log/nfs.log 2>&1
 
 pid_guard() {
   echo "------------ STARTING `basename $0` at `date` --------------" | tee /dev/stderr
